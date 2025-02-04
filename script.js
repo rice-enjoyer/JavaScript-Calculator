48703998393698fc9f77a8ff13fb1c6b92ec1ae8
rice enjoyer <arjinggo2010@gmail.com>
2025-02-04 11:49:56 +0530

    Update script.js
    Signed-off-by: rice enjoyer <arjinggo2010@gmail.com>

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ function clearDisplay() {
 
 function calculate() {
   try {
-    display.value = eval(display.value);
+    display.value = eval(display.value); /* IMPORTANT NOTE: eval() allows you to execute a string of code as if it were actual JavaScript code.
+While it is a useful tool, it is a security risk. I would recommend using eval() ONLY for learning purposes and not any actual production code.*/
   }
   catch(error) {
     display.value = "Error";
